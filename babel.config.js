9d80816541de2860e8a6b441a981f02313bc96c5
malixia <malixia@ruubypay.com>
2024-03-05 14:37:47 +0800

    [fix] 配置 文件别名引入

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,16 @@ module.exports = {
                     "REACT_APP_ENV"
                 ]
             }
+        ],
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+                alias: {
+                    '@': ['./src']
+                }
+            }
         ]
     ]
 };
